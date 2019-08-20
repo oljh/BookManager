7a2afa8a22918a594e113c0edf3270f554287c2f
oljh <https://github.com/oljh/MyCheese.git>
2019-08-20 16:51:34 +0300

    webApp

---
+++ src/main/java/dao/BookDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import model.Book;
+
+import java.util.List;
+
+public interface BookDao {
+    public void addBook(Book book);
+    public void updateBook(Book book);
+    public void removeBook(int id);
+    public Book getBookById(int id);
+    public List<Book> listBook();
+
+}
---
+++ src/main/java/dao/BookDaoImpl.java
@@ -0,0 +1,47 @@
+package dao;
+
+import model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class BookDaoImpl implements BookDao {
+    private static final Logger logger = LoggerFactory.getLogger(BookDaoImpl.class);
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void addBook(Book book) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.persist(book);
+        logger.info("Book sucessfully saved. Book details:" + book);
+    }
+
+    @Override
+    public void updateBook(Book book) {
+
+    }
+
+    @Override
+    public void removeBook(int id) {
+
+    }
+
+    @Override
+    public Book getBookById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Book> listBook() {
+        return null;
+    }
+}
